95da4a1f976074530daaf837c7b8f8486b095e90
Mar'i Fauzan <marifauzan20@gmail.com>
2021-10-21 20:07:58 +0700

    add func getFoods & getFoodById

--- helper.js
+++ helper.js
@@ -1,1 +1,23 @@
-console.log("This is helper");
+export const getFoods = async () => {
+  try {
+    const food = await fetch(
+      "https://616981a909e030001712c409.mockapi.io/foods"
+    );
+    return food.json();
+  } catch (error) {
+    console.log("getFoods", error);
+    throw error;
+  }
+};
+
+export const getFoodById = async (user_id) => {
+  try {
+    const food = await fetch(
+      `https://616981a909e030001712c409.mockapi.io/foods/${user_id}`
+    );
+    return food.json();
+  } catch (error) {
+    console.log("getFoodById", error);
+    throw error;
+  }
+};
